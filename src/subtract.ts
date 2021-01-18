85a67f28c08b7293788eea39b38f27b001c8492f
Maciej Kucaba <kucabix@Mac-mini-Maciej.local>
2021-01-18 15:20:05 +0100

    Api Extractor basic setup

---
+++ src/subtract.ts
@@ -0,0 +1,12 @@
+/**
+ * A simple subtracting function
+ * 
+ * @param a - first number
+ * @param b - second number
+ * 
+ * @beta
+  */
+export function subtract(a: number, b: number): number {
+  return a - b;
+}
+
